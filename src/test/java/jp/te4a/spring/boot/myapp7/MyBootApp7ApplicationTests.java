61493396c4fceb469b61b8d08f7b992424602a6e
YudaiSaito <tejc190711@gmail.com>
2022-05-24 12:08:25 +0900

    Change: 課題7へリネーム

--- src/test/java/jp/te4a/spring/boot/myapp7/MyBootApp7ApplicationTests.java
+++ src/test/java/jp/te4a/spring/boot/myapp7/MyBootApp7ApplicationTests.java
@@ -1,10 +1,10 @@
-package jp.te4a.spring.boot.myapp5;
+package jp.te4a.spring.boot.myapp7;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyBootApp5ApplicationTests {
+class MyBootApp7ApplicationTests {
 
 	@Test
 	void contextLoads() {
