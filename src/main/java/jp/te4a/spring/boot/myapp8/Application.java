21edf53b304f05840edd955b3cb6aee022c6d488
YudaiSaito <tejc190711@gmail.com>
2022-05-25 09:33:24 +0900

    Change: 課題8用にリネーム

--- src/main/java/jp/te4a/spring/boot/myapp8/Application.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/Application.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp7;
+package jp.te4a.spring.boot.myapp8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
