25f7c9d482d39710c58bc96c0a63f405cb14a28c
YudaiSaito <tejc190711@gmail.com>
2022-05-25 11:22:56 +0900

    Change: JPAepositoryに対応した仕様に

--- src/main/java/jp/te4a/spring/boot/myapp9/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookController.java
@@ -45,7 +45,7 @@ public class BookController {
     }
 
     @PostMapping(path = "delete")
-    String delete(@RequestParam Integer id) {
+    String delete(@RequestParam BookBean id) {
         bookService.delete(id);
         return "redirect:/books";
     }
--- src/main/java/jp/te4a/spring/boot/myapp9/BookService.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookService.java
@@ -2,6 +2,8 @@ package jp.te4a.spring.boot.myapp9;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,25 +12,24 @@ import org.springframework.stereotype.Service;
 public class BookService {
     @Autowired
     BookRepository bookRepository;
+    
     public BookForm create(BookForm bookForm) {
-        bookForm.setId(bookRepository.getBookId());
         BookBean bookBean = new BookBean();
         BeanUtils.copyProperties(bookForm, bookBean);
-        bookRepository.create(bookBean);
+        bookRepository.save(bookBean);
         return bookForm;
-    }
+    }     
 
     public BookForm update(BookForm bookForm) {
         BookBean bookBean = new BookBean();
         BeanUtils.copyProperties(bookForm, bookBean);
-        bookRepository.update(bookBean);
+        bookRepository.save(bookBean);
         return bookForm;
     }
-  
-    public void delete(Integer id) { 
-      bookRepository.delete(id);
+        
+    public void delete(BookBean id) { 
+        bookRepository.delete(id);
     }
-  
   
     public List<BookForm> findAll() {
         List<BookBean> beanList = bookRepository.findAll();
@@ -42,7 +43,7 @@ public class BookService {
     }
 
     public BookForm findOne(Integer id) {
-        BookBean bookBean = bookRepository.findOne(id);
+        Optional<BookBean> bookBean = bookRepository.findById(id);
         BookForm bookForm = new BookForm();
         BeanUtils.copyProperties(bookBean, bookForm);
         return bookForm;
