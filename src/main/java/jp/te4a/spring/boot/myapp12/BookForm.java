3bbb182d60998f8849c7f4a9bb8e78a1f08823ae
YudaiSaito <tejc190711@gmail.com>
2022-06-07 11:57:24 +0900

    Add: 著者のバリデーションチェック

--- src/main/java/jp/te4a/spring/boot/myapp12/BookForm.java
+++ src/main/java/jp/te4a/spring/boot/myapp12/BookForm.java
@@ -9,13 +9,14 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class BookForm {
-  private Integer id ;
-  @NotNull
-  @Size(min = 3)
-  private String title;
-  @Size(min = 3, max = 20)
-  private String writter;
-  private String publisher;
-  @Min(0)
-  private Integer price;
+    private Integer id ;
+    @NotNull
+    @Size(min = 3)
+    private String title;
+    @Size(min = 3, max = 20)
+    @Writter(param="東北タロウ")
+    private String writter;
+    private String publisher;
+    @Min(0)
+    private Integer price;
 }
