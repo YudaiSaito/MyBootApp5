f0349472ff1f239f574027a86ec21a5efb6f485e
YudaiSaito <tejc190711@gmail.com>
2022-06-24 14:52:37 +0900

    課題13

---
+++ src/main/java/jp/te4a/spring/boot/myapp13/controller/BookController.java
@@ -0,0 +1,66 @@
+package jp.te4a.spring.boot.myapp13.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jp.te4a.spring.boot.myapp13.bean.BookBean;
+import jp.te4a.spring.boot.myapp13.form.BookForm;
+import jp.te4a.spring.boot.myapp13.service.BookService;
+
+@Controller
+@RequestMapping("books")
+public class BookController {
+    @Autowired
+    BookService bookService;
+    @ModelAttribute 
+    BookForm setUpForm() {
+      return new BookForm();
+    }
+
+    @GetMapping
+    String list(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books/list";
+    }
+
+    @PostMapping(path="create")
+    String create(@Validated BookForm form, BindingResult result , Model model) {
+      if(result.hasErrors()) {
+        return list(model);
+      }
+      bookService.create(form);
+      return "redirect:/books";
+    }
+    
+    @PostMapping(path = "edit")
+    String edit(@RequestParam Integer id, @Validated BookForm form, BindingResult result) {
+        if(result.hasErrors()) {
+            return editForm(id, form);
+        }
+        bookService.update(form);
+        return "redirect:/books";
+    }
+
+    private String editForm(Integer id, BookForm form) {
+        return null;
+    }
+
+    @PostMapping(path = "delete")
+    String delete(@RequestParam BookBean id) {
+        bookService.delete(id);
+        return "redirect:/books";
+    }
+    
+    @PostMapping(path = "edit", params = "goToTop")
+    String goToTop() {
+        return "redirect:/books";
+    }
+}
---
+++ src/main/java/jp/te4a/spring/boot/myapp13/form/BookForm.java
@@ -0,0 +1,24 @@
+package jp.te4a.spring.boot.myapp13.form;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import jp.te4a.spring.boot.myapp13.validate.Writter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class BookForm {
+    private Integer id ;
+    @NotNull
+    @Size(min = 3)
+    private String title;
+    @Size(min = 3, max = 20)
+    @Writter(param="東北タロウ")
+    private String writter;
+    private String publisher;
+    @Min(0)
+    private Integer price;
+}
